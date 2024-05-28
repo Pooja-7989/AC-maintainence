525855925e3a8cdf88fa71283a94a707761fc1ab
Pooja Chennam <161332696+Pooja-7989@users.noreply.github.com>
2024-05-28 18:58:00 +0530

    Add files via upload

---
+++ ac/ac.js
@@ -0,0 +1,21 @@
+var  cleaning = document.getElementById('cleaning');
+var  repair = document.getElementById('repair');
+var gas = document.getElementById('gas');
+var relocation = document.getElementById('relocation');
+var  filter = document.getElementById('filter');
+var maintenance = document.getElementById('maintenance');
+
+function btnclick(event) {
+    const checkbox = [cleaning, repair, gas, relocation, filter,maintenance];
+    var checkcount=0
+    var amount = 0;
+    event.preventDefault();
+    for (let i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked) {
+            amount += parseInt(checkbox[i].value);
+            checkcount = checkcount+1
+        }
+    }
+    
+    document.getElementById('result').innerHTML = 'Total Estimate cost: ' + amount + "/-";
+}
